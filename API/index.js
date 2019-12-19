b7cfeea78a5b30971cd44339fda921b00321f1a5
kushanshamika <shamikakushan@gmail.com>
2019-12-19 19:42:54 +0000

    Develop Add Answer API

--- API/index.js
+++ API/index.js
@@ -48,3 +48,45 @@ app.post('/api/user',(req,res)=>{
 	console.log(err)
 });
 });
+
+function makeid(length) {
+   var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
+
+app.post('/api/addquiz',(req,res)=>{
+	var id = makeid(5);
+	var values = [id,req.body.q,req.body.a1,req.body.a2,req.body.a3,req.body.a4,req.body.cAns];
+	var sql = 'INSERT INTO quiz VALUES (?)';
+	conn.query(sql,[values],(err,rows,field)=>{
+	if(!err)
+	res.send({"id":id})
+	else
+	res.send(err)
+})
+})
+
+app.post('/api/quiz',(req,res)=>{
+	var values = [req.body.userID,req.body.quizID];
+	var sql = 'SELECT * FROM answer WHERE userID = ? AND quizID = ?';
+	conn.query(sql,[values],(err,rows,field)=>{
+	if(rows){
+//	var result = Object.assign({},{"status":true},rows[0]);
+//	res.send(result);
+	res.send({"status":false})
+	}else{
+	var values = [req.body.quizID];
+	var sql = 'SELECT * FROM quiz WHERE quizID = ?';
+	conn.query(sql,[values],(err,rows,field)=>{
+	var result = Object.assign({},{"status":true},rows[0]);
+        res.send(result);
+})
+}
+})
+})
+
